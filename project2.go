cbd3105ebefbd22ae2647dc71c4b8d5790c1d836
Vlad <RVlad@luxoft.com>
2020-08-04 13:07:48 +0300

    Project 4

--- project2.go
+++ project2.go
@@ -1,72 +1,50 @@
 package main
 
 import (
-	"golang.org/x/tour/tree"
 	"fmt"
+	"sync"
+	"time"
 )
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int) {
-	
-	if (t.Left != nil) {
-		Walk(t.Left, ch)
+func Get(c chan int, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	for x := range c {
+		fmt.Println(x)
 	}
-	ch <- t.Value
-	if (t.Right != nil) {
-		Walk(t.Right, ch)
-	}
+
 }
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-
-func Same(t1, t2 *tree.Tree) bool {
-	c1 := make(chan int)
-	c2 := make(chan int)
-
-	go Walk(t1, c1)
-	go Walk(t2, c2)
-
-	a1Array := make([]int, 0, 10)
-	a2Array := make([]int, 0, 10)
-
-	for len(a1Array) != 10 || len(a2Array) != 10 {
-		select {
-			case x := <- c1:
-				a1Array = append(a1Array, x)
-			case y := <- c2:
-				a2Array = append(a2Array, y)
-		}
+func Put(c chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(c)
+	for i := 0; i < 5; i++ {
+		// c <- i
 	}
-	for i := 0; i < 10; i++ {
-		if a1Array[i] != a2Array[i] {
-			return false
-		}
-	}
-	return true
 }
-
-/*
-	I am implementing the exercise described here: https://go-tour-ro.appspot.com/#68
-	I must tell if two trees have the same values, and, in order to do this, I am using 
-	goroutines and channels
-
-*/
 
 func main() {
 
-	/*t := tree.New(2)
-	ch := make(chan int)
-
-	go Walk(t, ch)
-
-	for i := 0; i < 10; i++ {
-		x := <- ch
-		fmt.Println(x)
-	}*/
-
-	fmt.Println(Same(tree.New(1), tree.New(1)))
-	fmt.Println(Same(tree.New(1), tree.New(2)))
-
+	chanOwner := func() <-chan int {
+		results := make(chan int, 5) 
+		go func() {
+			defer close(results)
+			for i := 0; i <= 20; i++ {
+				results <- i
+				fmt.Printf("Added value %d\n", i)
+			}
+		}()
+		return results
+	}
+	
+	consumer := func(results <-chan int) { 
+		for result := range results {
+			fmt.Printf("Received: %d\n", result)
+			time.Sleep(1 * time.Second)
+		}
+		fmt.Println("Done receiving!")
+	}
+	
+	results := chanOwner()        
+	consumer(results)
 }
---
+++ project4.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandomNumber() int {
+	return rand.Int()
+}
+
+func generatePrime(
+	done <- chan interface{},
+	random <- chan int,
+	N int) chan int {
+
+		output := make(chan int)
+		go func() {
+			defer close(output)
+			for i := 0; i < N; i++ {
+				select {
+				case <- done:
+					return
+				case x := <- random:
+					found := false
+					for i := 2; i * i <= x; i++ {
+						if x % i == 0 {
+							found = true
+							break
+						}
+					}
+					if !found {
+						output <- x
+					}
+				}
+			}
+		}()
+		
+		return output
+	}
+
+func generateNumbers(
+	done <-chan interface{},
+	values ...int) chan int {
+		
+		valueChan := make(chan int)
+		
+		go func() {
+
+			defer close(valueChan)
+			for {
+				for _, value := range values {
+					select {
+					case <- done:
+						return
+					case valueChan <- value:
+					}
+				}
+			}
+		}()
+
+		return valueChan
+	}
+
+func generateNNumbers(
+	done <-chan interface{},
+	N int,
+	values ...int) chan int {
+
+		valueChan := make(chan int)
+		
+		go func() {
+
+			defer close(valueChan)
+
+			for i := 0; i < N; i++ {
+				for _, value := range values {
+					select {
+					case <- done:
+						return
+					case valueChan <- value:
+					}
+				}
+			}
+		}()
+
+		return valueChan
+}
+
+func fannIn(
+	done chan interface{},
+	channels ...chan int) chan int {
+		fannOut := make(chan int)
+		numChanClosed := 0
+		totalChan := len(channels)
+		for _, c := range channels {
+			go func() {
+				for value := range c {
+					select {
+					case <- done:
+						return
+					default:
+						fannOut <- value
+					}
+				}
+				numChanClosed = numChanClosed + 1
+				if numChanClosed == totalChan {
+					close(fannOut)
+				}
+			}()
+		}
+		
+		return fannOut
+
+	}
+
+
+func main() {
+
+	/*
+		I compared the execution time between a function that writes data in a channel
+		and use that data vs 4 channels that uses the same amount of data and a channel
+		that listen on the 4 channels.
+		The first execution time is ~ 3.1 ms
+		The second execution time is ~2.0 ms
+
+	*/
+
+	start := time.Now()
+	done := make(chan interface{})
+	defer close(done)
+
+	outChan1 := generatePrime(done, generateNNumbers(done, 2050, generateRandomNumber(), 
+									generateRandomNumber(), generateRandomNumber()), 2050)
+	for x := range outChan1 {
+		fmt.Println(x)
+	}
+
+	fmt.Println(time.Since(start))
+
+	start = time.Now()
+
+	var chans [4]chan int
+	for i := range chans {
+	    chans[i] = generatePrime(done, generateNNumbers(done, 500, generateRandomNumber(), 
+	   				generateRandomNumber(), generateRandomNumber()), 500)
+	}
+
+	fannOutChan := fannIn(done, chans[0], chans[1], chans[2], chans[3])
+
+	for x := range fannOutChan {
+		fmt.Println(x)
+	}
+	fmt.Println(time.Since(start))
+
+
+}
